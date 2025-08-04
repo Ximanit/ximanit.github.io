6c4e7a320f32adc2964546101bafd61d4d1bd5d7
Ximanit <122241859+Ximanit@users.noreply.github.com>
2025-08-04 02:56:13 +0000

    Сделал то что хотел шеф

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,31 @@ import { ProcessSection } from './components/ProcessSection';
 import { StatsSection } from './components/StatsSection';
 import { ContactSection } from './components/ContactSection';
 import { Footer } from './components/Footer';
+import { useState } from 'react';
 
 export default function App() {
+	const [hide, setHide] = useState(true);
+
 	return (
-		<div className="min-h-screen bg-white">
-			<Header />
-			<main>
-				<HeroSection />
-				<AboutSection />
-				<ServicesSection />
-				<ProcessSection />
-				<StatsSection />
-				<ContactSection />
-			</main>
-			<Footer />
-		</div>
+		<>
+			{hide ? (
+				<button onClick={(prev) => setHide(!prev)}>
+					<img src="/1.png" width={1920} height={1080} alt="old-site" />
+				</button>
+			) : (
+				<div className="min-h-screen bg-white">
+					<Header />
+					<main>
+						<HeroSection />
+						<AboutSection />
+						<ServicesSection />
+						<ProcessSection />
+						<StatsSection />
+						<ContactSection />
+					</main>
+					<Footer />
+				</div>
+			)}
+		</>
 	);
 }
