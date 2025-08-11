e7bd1105cfd9345e5c4b1b968502a531baca18d7
Ximanit <122241859+Ximanit@users.noreply.github.com>
2025-08-11 04:42:37 +0000

    3

---
+++ src/constants.js
@@ -0,0 +1,30 @@
+export const stats = [
+	{
+		icon: TrendingUp,
+		value: '2M+',
+		label: 'Тонн отходов переработано',
+		description: 'За все время работы компании',
+		color: 'text-blue-600',
+	},
+	{
+		icon: Leaf,
+		value: '85%',
+		label: 'Доля вторичной переработки',
+		description: 'От общего объема принимаемых отходов',
+		color: 'text-green-600',
+	},
+	{
+		icon: Users,
+		value: '500+',
+		label: 'Довольных клиентов',
+		description: 'Частных лиц и предприятий',
+		color: 'text-purple-600',
+	},
+	{
+		icon: Award,
+		value: '15',
+		label: 'Лет успешной работы',
+		description: 'Опыт и профессионализм',
+		color: 'text-orange-600',
+	},
+];
